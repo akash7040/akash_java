d083cabf730971bbae19c67f654b618c55da034f
akash7040 <akashav7040@gmail.com>
2026-09-18 23:34:31 +0530

    added to package

--- src/main/java/com/akash/begin/Basic.java
+++ src/main/java/com/akash/begin/Basic.java
@@ -1,6 +1,8 @@
+package com.akash.begin;
+
 public class Basic {
 
     public static void main(String[] args) {
-       // System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }

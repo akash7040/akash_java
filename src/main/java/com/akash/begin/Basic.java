4fdb1e1d3eeb8d6cb34c272df0762d02890c9015
akash7040 <akashav7040@gmail.com>
2026-09-19 00:47:10 +0530

    basnsat again

--- src/main/java/com/akash/begin/Basic.java
+++ src/main/java/com/akash/begin/Basic.java
@@ -5,5 +5,6 @@ public class Basic {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Akash Verma");
+        System.out.println("Basant");
     }
 }
